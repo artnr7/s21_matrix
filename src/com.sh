8af95db90497f8d87be7e7c8e440b2edd61b898f
sundaeka <artnr7@gmail.com>
2025-02-05 00:24:43 +0300

    Wed Feb  5 00:24:43 MSK 2025

---
+++ src/com.sh
@@ -0,0 +1,24 @@
+$date = date +%d.%m.%Y\ %H:%M
+
+
+while ! cd .. >/dev/null 2>&1; do
+    echo "add..."
+    sleep 2
+done
+
+while ! git add . >/dev/null 2>&1; do
+    echo "add..."
+    sleep 2
+done
+
+while ! git commit -m "$(date)" >/dev/null 2>&1; do
+    echo "commit..."
+    sleep 2
+done
+
+while ! git push origin develop >/dev/null 2>&1; do
+    echo "push..."
+    sleep 2
+done
+echo "Done."
+
